9aeb0a1b8a1fc7cee8071aa5e5cfb907543a93d9
Meash <m4rtinsch@gmail.com>
2015-06-11 08:18:44 +0200

    Implement sensoring other copters.

--- copter/sensors/SensorMock.py
+++ copter/sensors/SensorMock.py
@@ -1,4 +1,5 @@
 from sympy.geometry import *
+
 from copter.sensors.Sensor import Sensor
 
 __author__ = 'Martin'
@@ -8,26 +9,36 @@ class SensorMock(Sensor):
     def __init__(self, sensor_range, boundary, other_copters):
         self.sensor_range = sensor_range
         self.boundary_lines = self.get_boundary_lines(boundary)
-        self.other_copters_circles = self.get_copters_circles(other_copters)
+        self.other_copters = other_copters
 
     def collect_obstacles_closest_points(self, position):
         vision_area = Circle(Point(position.x, position.y), self.sensor_range)
         return self.get_closest_points(vision_area, self.boundary_lines)
 
-    def collect_other_copters_closest_points(self, position):
-        vision_area = Circle(Point(position.x, position.y), self.sensor_range)
-        return self.get_closest_points(vision_area, self.other_copters_circles)
-
     @staticmethod
     def get_closest_points(vision_area, entities):
         closest_points = []
         for entity in entities:
-            intersecting_points = intersection(entity, vision_area)
+            intersecting_points = vision_area.intersection(entity)
             if len(intersecting_points) == 1:
                 closest_points.append(intersecting_points[0])
-            if len(intersecting_points) == 2:
+            elif len(intersecting_points) == 2:
                 center = (intersecting_points[0] + intersecting_points[1]) / 2
                 closest_points.append(center)
+        return closest_points
+
+    def collect_other_copters_closest_points(self, position):
+        position_point = Point(position.x, position.y)
+        other_copters_circles = self.get_copters_circles(self.other_copters)
+        closest_points = []
+        for other_copter_circle in other_copters_circles:
+            # use line connection since vision_area could overlap with other copter,
+            # without intersecting with it
+            connection = Line(position_point, other_copter_circle.center)
+            intersecting_points = connection.intersection(other_copter_circle)
+            closest_point = intersecting_points[0]
+            if position_point.distance(closest_point) <= self.sensor_range:
+                closest_points.append(closest_point)
         return closest_points
 
     @staticmethod
