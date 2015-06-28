999dc7e65408b6e0480440a8f79a15070a42d506
Meash <m4rtinsch@gmail.com>
2015-06-28 22:51:23 +0200

    Draw the connectors from the copter to obstacles or other copters.

--- ui/ObstaclesDrawer.py
+++ ui/ObstaclesDrawer.py
@@ -5,20 +5,31 @@ __author__ = 'Martin'
 
 
 class ObstaclesDrawer(ObstacleListener):
-    def __init__(self, canvas):
+    def __init__(self, copter, canvas):
+        self.copter = copter
         self.canvas = canvas
         self.point_helper = PointHelper(canvas)
         self.drawn_obstacles = []
+        self.drawn_connectors = []
 
     def on_obstacles_update(self, obstacle_closest_points):
         drawn_obstacles = list(self.drawn_obstacles)
+        drawn_connectors = list(self.drawn_connectors)
         self.draw_obstacles(obstacle_closest_points)
+        for drawn_connector in drawn_connectors:
+            self.canvas.delete(drawn_connector)
+            self.drawn_connectors.remove(drawn_connector)
         for drawn_obstacle in drawn_obstacles:
             self.canvas.delete(drawn_obstacle)
             self.drawn_obstacles.remove(drawn_obstacle)
 
     def draw_obstacles(self, obstacle_closest_points):
         obstacle_size = 5 / 2
+
+        copter_x = self.copter.position.x
+        copter_y = self.copter.position.y
+        copter_y = self.point_helper.invert_y(copter_y)
+
         for obstacle_point in obstacle_closest_points:
             x = obstacle_point.x
             y = obstacle_point.y
@@ -29,3 +40,9 @@ class ObstaclesDrawer(ObstacleListener):
                 fill="red"
             )
             self.drawn_obstacles.append(drawn_obstacle)
+
+            drawn_connector = self.canvas.create_line(
+                x, y, copter_x, copter_y,
+                fill="darkgray"
+            )
+            self.drawn_connectors.append(drawn_connector)
--- ui/OtherCoptersDrawer.py
+++ ui/OtherCoptersDrawer.py
@@ -5,19 +5,31 @@ __author__ = 'Martin'
 
 
 class OtherCoptersDrawer(OtherCopterListener):
-    def __init__(self, canvas):
+    def __init__(self, copter, canvas):
+        self.copter = copter
         self.canvas = canvas
         self.point_helper = PointHelper(canvas)
         self.drawn_copters = []
+        self.drawn_connectors = []
 
     def on_other_copters_update(self, other_copters_closest_points):
         drawn_copters = list(self.drawn_copters)
+        drawn_connectors = list(self.drawn_connectors)
         self.draw_copters(other_copters_closest_points)
+        for drawn_connector in drawn_connectors:
+            self.canvas.delete(drawn_connector)
+            self.drawn_connectors.remove(drawn_connector)
         for drawn_copter in drawn_copters:
             self.canvas.delete(drawn_copter)
+            self.drawn_copters.remove(drawn_copter)
 
     def draw_copters(self, other_copters_closest_points):
         size = 5 / 2
+
+        copter_x = self.copter.position.x
+        copter_y = self.copter.position.y
+        copter_y = self.point_helper.invert_y(copter_y)
+
         for point in other_copters_closest_points:
             x = point.x
             y = point.y
@@ -27,3 +39,9 @@ class OtherCoptersDrawer(OtherCopterListener):
                 x + size, y + size,
                 fill="blue")
             self.drawn_copters.append(drawn_copter)
+
+            drawn_connector = self.canvas.create_line(
+                x, y, copter_x, copter_y,
+                fill="darkgray"
+            )
+            self.drawn_connectors.append(drawn_connector)
