0ded5665c8e674851a4790ddbd462c481876f8bb
Meash <m4rtinsch@gmail.com>
2015-06-28 23:38:21 +0200

    Fix termination of Looper timer by marking it as a daemon.

--- Looper.py
+++ Looper.py
@@ -7,22 +7,23 @@ class Looper:
     def __init__(self, timeout, copters):
         self.timeout = timeout
         self.copters = copters
-        self.run = True
+        self.timer = None
 
     def start(self):
         self.run_timer()
 
     def run_timer(self):
-        Timer(self.timeout, self.update).start()
+        self.timer = Timer(self.timeout, self.update)
+        self.timer.name = "Looper Timer"
+        self.timer.daemon = True
+        self.timer.start()
 
     def update(self):
-        if not self.run:
-            return
-
         for copter in self.copters:
             copter.update()
 
         self.run_timer()
 
     def stop(self):
-        self.run = False
+        if self.timer is not None:
+            self.timer.cancel()
